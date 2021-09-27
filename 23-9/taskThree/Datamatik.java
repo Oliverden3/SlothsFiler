ea77acbed72750ab9e299d9982964e8de776ca9d
Oliverden3 <oliversjensen@hotmail.com>
2021-09-27 11:20:34 +0200

    Bank

--- 23-9/taskThree/Datamatik.java
+++ 23-9/taskThree/Datamatik.java
@@ -6,10 +6,9 @@ public static void main(String[] args) {
   Student student1 = new Student("Oliver", 22, false,"TeamA");
   Student student2 = new Student("Nicolai", 19, false, "TeamA");
   System.out.println(teacher.name);
-  System.out.println(student2.name);
-  System.out.println(student1.name);
-  System.out.println(student1.datamatikerTeam);
-  System.out.println(student2.datamatikerTeam);
+  System.out.println(student2.name + " " + student2.datamatikerTeam);
+  System.out.println(student1.name + " " + student1.datamatikerTeam);
+ 
 }
 
 
