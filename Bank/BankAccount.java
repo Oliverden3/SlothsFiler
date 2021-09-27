e4da507d82154dd757399fdb74ecadb6977ef983
Oliverden3 <oliversjensen@hotmail.com>
2021-09-27 11:22:30 +0200

    newBank

---
+++ Bank/BankAccount.java
@@ -0,0 +1,12 @@
+class BankAccount {
+
+	float balance =1000;
+	String owner = "Oliver";
+	float[] transactions = new float[5];
+
+
+public BankAccount(float balance, String owner, float[] transactions)
+	this.balance = balance;
+	this.owner = owner;
+	this.transactions = transactions;
+}
---
+++ Bank/Main.java
@@ -0,0 +1,12 @@
+class Main {
+
+
+public static void main(String args[]){
+	BankAccount a1 = new BankAccount();
+	BankAccount a2 = new BankAccount();
+	BankAccount a3 = new BankAccount();
+	System.out.println(a1 + " : " + a2 +": " + a3);
+}
+
+}
+
